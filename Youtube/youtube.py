f78adb550da13391c0be7e3ca2e7fba60911c49f
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-16 13:01:16 +0530

    Update youtube.py

--- Youtube/youtube.py
+++ Youtube/youtube.py
@@ -42,6 +42,8 @@ async def process_youtube_link(client, message):
             if title:
                 ydl.download([youtube_link])
                 uploading_msg = await message.reply_text("Uploading video...")
+                video_extension = info_dict.get('ext', 'mp4')
+                video_filename = f"downloaded_video_{info_dict['id']}.{video_extension}"
                 video_filename = f"downloaded_video_{info_dict['id']}.mp4"
                 sent_message = await client.send_video(message.chat.id, video=open(video_filename, 'rb'), caption=title)
 
